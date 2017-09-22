b6181f959382a8070156851b4e6e61732c787d9e
amosnistrian <amosnistrian@gmail.com>
2017-09-22 16:07:02 -0700

    adding rating control

---
+++ AdaptiveUITextView/StackCell.swift
@@ -0,0 +1,17 @@
+//
+//  StackCell.swift
+//  AdaptiveUITextView
+//
+//  Created by Amos  on 9/22/17.
+//  Copyright © 2017 Arun. All rights reserved.
+//
+
+import UIKit
+
+class StackCell: UITableViewCell {
+
+    @IBOutlet weak var label: UILabel!
+    
+    @IBOutlet weak var stack: RatingControl!
+
+}
